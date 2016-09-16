48ecc00798250cc947189f5eb0c87d6918d78724
J <kiendo95@gmail.com>
2016-09-17 01:26:10 +0700

    Connect db works

--- includes/sql_connection.inc.php
+++ includes/sql_connection.inc.php
@@ -6,7 +6,6 @@ $username 	= "user=rgsyjaksdrabim ";
 $password 	= "password=JrI60M9r-gKk2xcgNS7QtJempJ ";
 $option = "sslmode=require ";
 
-$url = $host + $database + $username + $port + $option + $password;
-var_dump($url);
+$url = "host=ec2-54-225-72-148.compute-1.amazonaws.com dbname=dmm21h6vgpnuo port=5432 user=rgsyjaksdrabim password=JrI60M9r-gKk2xcgNS7QtJempJ sslmode=require";
 $DBC = pg_connect($url);
 ?>
