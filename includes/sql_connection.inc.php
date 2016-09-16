52aeb3c143c6fba85ccbc18c5fda45374689fada
J <kiendo95@gmail.com>
2016-09-16 16:29:57 +0700

    Add space in config db

--- includes/sql_connection.inc.php
+++ includes/sql_connection.inc.php
@@ -1,10 +1,10 @@
 <?php
-  $host 		= "host=ec2-54-225-72-148.compute-1.amazonaws.com";
- $port 		= "port=5432";
- $database 	= "dbname=dmm21h6vgpnuo";
-$username 	= "user=rgsyjaksdrabim";
-$password 	= "password=JrI60M9r-gKk2xcgNS7QtJempJ";
-$option = "sslmode=require";
+  $host 		= " host=ec2-54-225-72-148.compute-1.amazonaws.com ";
+ $port 		= "port=5432 ";
+ $database 	= "dbname=dmm21h6vgpnuo ";
+$username 	= "user=rgsyjaksdrabim ";
+$password 	= "password=JrI60M9r-gKk2xcgNS7QtJempJ ";
+$option = "sslmode=require ";
 
 $url = $host + $database + $username + $port + $option + $password;
 var_dump($url);
